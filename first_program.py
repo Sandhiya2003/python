a1e87687026d51bea5f2084917794a46a2cee032
Sandhiya <sandhiya.s2707@gmail.com>
2021-03-28 05:36:12 -0700

    Access token

--- first_program.py
+++ first_program.py
@@ -1,6 +1,9 @@
-name = "Krishbby"
+#481c9df9e59d562e5f3a7eb7d6692efe6dba2f1a
+
+ame = "Krishbby"
 name_1 = "I'm Sandhiya"
 print("Hello "  + name + ", " + name_1)
 
 hi = print("Hi Sandhiya, how are you?")
+
 
